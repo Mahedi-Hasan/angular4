0685322abcea448cff1473586c5aa7835774cbaf
mahedi <mahedi.modhail@gmail.com>
2017-10-16 16:02:44 +0600

    adding-data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import {Friend} from './Friend';
+import {NgForm} from '@angular/forms';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+
+@Injectable()
 
 @Component({
   selector: 'app-root',
@@ -7,5 +14,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Testy';
-  description = 'Very good !!!'
+  description = 'Very good !!!';
+
+  constructor(private _http: Http){}
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  onSubmit(form: NgForm): Promise <Friend>{
+    return this._http.post('http://laramang.com/public/add-friend', JSON.stringify(form.value), {headers: this.headers})
+     .toPromise()
+             .then(res => res.json().data)
+              .catch(this.handleError);
+}
+private handleError(error: any): Promise<any> {
+  console.error('An error occurred', error); // for demo purposes only
+  return Promise.reject(error.message || error);
+  }
+
 }
